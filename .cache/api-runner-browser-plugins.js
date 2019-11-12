85a4f0767c6fc3675bb5210521a1e9219eea58bf
Maverick100199 <1siddharthkulkarni007@gmail.com>
2019-11-12 12:52:53 +0530

    enabling netlify forms

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/home/siddharth/Desktop/projects/github projects/portfolio-website/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
--- src/components/contact.js
+++ src/components/contact.js
@@ -10,7 +10,7 @@ const Contact = () => (
       <p><input type='text' placeholder='Name' name='name' required /></p>
       <p><input type='email' placeholder='Email' name='email' required /></p>
       <p><textarea placeholder='Message' rows='5' name='message' required></textarea></p>
-      <p><button type='submit' className='button' value='SUBMIT'></button></p>
+      <p><input type='submit' className='button' value='SUBMIT' /></p>
     </form></center>
   </div>
 )
