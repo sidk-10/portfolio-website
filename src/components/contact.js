855a4b1fc5c24941f612b48d99295f0e81307426
Maverick100199 <1siddharthkulkarni007@gmail.com>
2019-11-12 11:43:29 +0530

    enabling netlify forms

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,12 +6,12 @@ const Contact = () => (
     <div className='contact_title'>CONTACT</div>
     <center><div className='highlight2'></div></center>
     <center><div id='q'>Have a question or want to work together??</div></center>
-    <center><form name='contact_me' action='' method='post' data-netlify='true'>
-      <input type='text' placeholder='Name' required></input><br></br>
-      <input type='email' placeholder='Email' required></input><br></br>
-      <textarea placeholder='Message' rows='5' required></textarea><br></br>
+    <center><form name='contact_me' action='' method='POST' data-netlify='true'>
+      <input type='text' placeholder='Name' name='name' required></input><br></br>
+      <input type='email' placeholder='Email' name='email' required></input><br></br>
+      <textarea placeholder='Message' rows='5' name='message' required></textarea><br></br>
       <input type='submit' className='button' value='SUBMIT'></input>
-    </form></center>
+    </form></center>>
   </div>
 )
 
